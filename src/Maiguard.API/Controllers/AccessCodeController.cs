2cdc9e181036e7824a31dd60f6d9583581081d2a
olumuyiwa-agboola <agboolaod@gmail.com>
2024-09-19 16:32:13 +0100

    Inital setup for API project

---
+++ src/Maiguard.API/Controllers/AccessCodeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Maiguard.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class AccessCodeController : ControllerBase
+    {
+        private readonly string _message = "This feature is yet to be implemented. Contributions to the development of this feature are welcome at https://github.com/olumuyiwa-agboola/maiguard-api";
+
+        [HttpPost]
+        [Route("Generate")]
+        public IActionResult GenerateAccessCode()
+        {
+            return Ok(new
+            {
+                message = _message
+            });
+        }
+
+        [HttpPost]
+        [Route("Verify")]
+        public IActionResult VerifyAccessCode()
+        {
+            return Ok(new
+            {
+                message = _message
+            });
+        }
+
+        [HttpDelete]
+        [Route("Cancel")]
+        public IActionResult CancelAccessCode()
+        {
+            return Ok(new
+            {
+                message = _message
+            });
+        }
+    }
+}

1e2209a0f7f3736809fd084837c6aacc74e61d7c
olumuyiwa-agboola <agboolaod@gmail.com>
2024-10-06 08:29:46 +0100

    Add documentation to resident activation and deactivation models

--- src/Maiguard.Core/Models/Residents/ResidentActivationRequest.cs
+++ src/Maiguard.Core/Models/Residents/ResidentActivationRequest.cs
@@ -7,15 +7,20 @@ using System.Threading.Tasks;
 namespace Maiguard.Core.Models.Residents
 {
     /// <summary>
+    /// Model for activating a resident account
     /// </summary>
     public class ResidentActivationRequest
     {
         /// <summary>
+        /// Resident Id
         /// </summary>
+        /// <example>RFHADI5188</example>
         public string ResidentId { get; set; } = string.Empty;
 
         /// <summary>
+        /// Community admin Id or "SELF" for self-activation
         /// </summary>
+        /// <example>SELF</example>
         public string ActivatedBy { get; set; } = string.Empty;
     }
 }
--- src/Maiguard.Core/Models/Residents/ResidentDeactivationRequest.cs
+++ src/Maiguard.Core/Models/Residents/ResidentDeactivationRequest.cs
@@ -1,15 +1,20 @@
 ﻿namespace Maiguard.Core.Models.Residents
 {
     /// <summary>
+    /// Model for deactivating a resident account
     /// </summary>
     public class ResidentDeactivationRequest
     {
         /// <summary>
+        /// Resident Id
         /// </summary>
+        /// <example>RFHADI5188</example>
         public string ResidentId { get; set; } = string.Empty;
 
         /// <summary>
+        /// Community admin Id or "SELF" for self-deactivation
         /// </summary>
+        /// <example>SELF</example>
         public string DeactivatedBy { get; set; } = string.Empty;
     }
 }
